6590bd712634aab61130058711a727e59df1f6aa
kealanheena <kealanheena@gmail.com>
2019-12-05 10:08:24 +0000

    update sql file

--- bookmark_manager/db/migrations/01_create_bookmarks_table.sql
+++ bookmark_manager/db/migrations/01_create_bookmarks_table.sql
@@ -1,1 +1,8 @@
+/* Active Database */
+CREATE DATABASE bookmark_manager;
+\c bookmark_mangager;
+CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
+/* Test Database */
+CREATE DATABASE bookmark_manager_test;
+\c bookmark_mangager;
 CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
