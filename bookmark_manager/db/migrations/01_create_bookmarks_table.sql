e8938850f1c0685a805d2414240e3b99652e7526
kealanheena <kealanheena@gmail.com>
2019-12-05 11:56:42 +0000

    add bootstrap

--- bookmark_manager/db/migrations/01_create_bookmarks_table.sql
+++ bookmark_manager/db/migrations/01_create_bookmarks_table.sql
@@ -2,6 +2,7 @@
 CREATE DATABASE bookmark_manager;
 \c bookmark_mangager;
 CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
+
 /* Test Database */
 CREATE DATABASE bookmark_manager_test;
 \c bookmark_mangager;
